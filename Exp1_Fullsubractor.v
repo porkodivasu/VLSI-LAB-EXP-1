2c8190213e166bae62dddc9f308d5062033d7be7
porkodivasu <160757120+porkodivasu@users.noreply.github.com>
2024-04-08 16:03:06 +0530

    Update Exp1_Fullsubractor.v

--- Exp1_Fullsubractor.v
+++ Exp1_Fullsubractor.v
@@ -1,7 +1,10 @@
-module fs(a,b,bin,d,bout);
-input a,b,bin;
-output d,bout;
-
-
-
+module fullsubtractor (a,b,c,difference,borrow);
+input a,b,c;
+output difference,borrow;
+wire w1,w2,w3;
+xor g1(w1,a,b);
+xor g2(difference,w1,c);
+and g3(w2,~a,b);
+and g4(w3,w1,c);
+or g5(borrow,w3,w2);
 endmodule
